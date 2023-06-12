9609557d2e25af8f62f8296c6af4080b5507ab19
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-06-12 12:44:40 +0530

    Time: 0 ms (100%), Space: 40.9 MB (57.62%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,16 +1,32 @@
 class Solution {
-  public List<String> summaryRanges(int[] nums) {
-    int start, end, n = nums.length, i = 0;
-    var ans = new ArrayList<String>();
-
-    while (i<n) {
-      for (start = end = nums[i]; i<n && end == nums[i]; i++, end++);
-
-      ans.add(
-        end == start + 1
-          ? "" + start
-          : String.format("%d->%d", start, end-1));
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if (nums.length == 0) return res;
+        int prev = nums[0];
+        StringBuilder sb = new StringBuilder();
+        sb.append(prev);
+        if (nums.length == 1) {
+            res.add(sb.toString());
+            return res;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            int len = String.valueOf(prev).length();
+            if (nums[i] - nums[i - 1] == 1) {
+                if (sb.length() == len) {
+                    sb.append("->");
+                    sb.append(nums[i]);
+                } else {
+                    sb.setLength(len + 2);
+                    sb.append(nums[i]);
+                }
+            } else if (nums[i] - nums[i - 1] != 1) {
+                res.add(sb.toString());
+                sb.setLength(0);
+                sb.append(nums[i]);
+                prev = nums[i];
+            }
+            if (i == nums.length - 1) res.add(sb.toString());
+        }
+        return res;
     }
-    return ans;
-  }
 }
