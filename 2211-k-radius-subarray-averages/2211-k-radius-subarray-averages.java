179d7d93b9382f69f7dabe52eeefa26b6fb2af88
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-06-20 13:30:18 +0530

    Time: 6 ms (100%), Space: 59.6 MB (84.6%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.java
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.java
@@ -1,23 +1,33 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        int n = nums.length;
-        int windowSize = 2 * k + 1;
-        int[] ans = new int[n];
-        Arrays.fill(ans,-1);
-        
-        if (n < windowSize) {
-            return ans;
+
+        int[] avgs = new int[nums.length];
+
+        Arrays.fill(avgs, -1);
+
+        if(2 * k + 1 > nums.length){
+            return avgs;
         }
-        
-        long[] prefixSum = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+
+        double sum = 0;
+
+        for(int i = 0; i < 2 * k + 1; i++){
+            sum += nums[i];
         }
-        
-        for (int i = k; i + k < n; ++i) {
-            ans[i] = (int) ((prefixSum[i + k + 1] - prefixSum[i - k]) / windowSize);
+
+        double avg = sum / (2 * k + 1);
+        avgs[k] = (int)avg;
+
+        for(int i = 2 * k + 1, c = 0; i < nums.length; i++, c++){
+            sum += nums[i];
+            sum -= nums[c];
+
+            avg = sum / (2 * k + 1);
+            avgs[k + c + 1] = (int)avg;
+
         }
-        
-        return ans;
+
+        return avgs;
+
     }
 }
