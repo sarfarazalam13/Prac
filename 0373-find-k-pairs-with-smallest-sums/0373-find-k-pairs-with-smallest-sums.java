e4efa7281be7f88a5308dd7dfdf0396b485f737b
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-06-27 15:23:38 +0530

    Time: 16 ms (99.73%), Space: 57.4 MB (57.83%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,35 +1,65 @@
-
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0 || k <= 0) {
-            return res;
-        }
-        
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] + a[1] - b[0] - b[1]);
-        Set<String> visited = new HashSet<>();
-        
-        heap.offer(new int[]{nums1[0], nums2[0], 0, 0});
-        visited.add("0,0");
-        
-        while (k > 0 && !heap.isEmpty()) {
-            int[] curr = heap.poll();
-            int i = curr[2];
-            int j = curr[3];
-            res.add(List.of(curr[0], curr[1]));
-            k--;
+
+        int len1 = nums1.length, len2 = nums2.length;
+
+        int left = nums1[0] + nums2[0];
+        int right = nums1[len1 - 1] + nums2[len2 - 1];
+        while (left <= right){
+            int middle = (int)(((long)left + (long)right) / 2);
             
-            if (i + 1 < nums1.length && !visited.contains((i + 1) + "," + j)) {
-                heap.offer(new int[]{nums1[i + 1], nums2[j], i + 1, j});
-                visited.add((i + 1) + "," + j);
-            }
-            
-            if (j + 1 < nums2.length && !visited.contains(i + "," + (j + 1))) {
-                heap.offer(new int[]{nums1[i], nums2[j + 1], i, j + 1});
-                visited.add(i + "," + (j + 1));
+            long cnt = getSmallerGreaterCnt(nums1, nums2, middle, k);
+            if (cnt < k){
+                left = middle + 1;
+            } else if (cnt > k){
+                right = middle - 1;
+            } else {
+                left = middle;
+                break;
             }
         }
-        
-        return res;
+        return getPairs(nums1, nums2, left, k);
+    }
+    int getSmallerGreaterCnt(int[] nums1, int[] nums2, int target, int k){
+        int previousRight = nums2.length - 1;
+        int cnt = 0;
+        for (int i = 0; i < nums1.length && nums1[i] + nums2[0] <= target; i++){
+            int left = 0, right = previousRight;
+            int pos = -1;
+            while (left <= right){
+                int middle = (left + right) / 2;
+                int sum = nums1[i] + nums2[middle];
+                if (sum <= target){
+                    pos = middle;
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+            if (pos >= 0){
+                cnt += pos + 1;
+                previousRight = pos;
+            }
+            if (cnt > k){
+                return cnt;
+            }
+        }
+        return cnt;
+    }
+    List<List<Integer>> getPairs(int[] nums1, int[] nums2, int targetSum, int k){
+        List<List<Integer>> pairs = new ArrayList();
+        for (int i = 0; i < nums1.length; i++){
+            for (int j = 0; j < nums2.length && nums1[i] + nums2[j] < targetSum; j++){
+                pairs.add(Arrays.asList(nums1[i], nums2[j]));
+            }
+        }
+        for (int i = 0; i < nums1.length; i++){
+            for (int j = 0; j < nums2.length && nums1[i] + nums2[j] <= targetSum && pairs.size() < k; j++){
+                if (nums1[i] + nums2[j] == targetSum){
+                    pairs.add(Arrays.asList(nums1[i], nums2[j]));
+                }
+            }
+        }
+        return pairs;
     }
 }
