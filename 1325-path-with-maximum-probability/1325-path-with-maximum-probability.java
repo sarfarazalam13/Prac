8e9b07ec8062813b828f284ccc26ecd0a2b08067
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-06-28 05:44:09 +0530

    Time: 10 ms (97.43%), Space: 55 MB (80.64%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,58 +1,24 @@
-import java.util.*;
-class pair{
-int key;
-double value;
-
-public pair(int key,double value){
-    this.key = key;
-    this.value = value;
-}
-
-public int getKey(){return key;}
-public double getValue(){return value;}
-}
 class Solution {
-public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-
-    ArrayList<ArrayList<pair>> adj = new ArrayList<>();
-    for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-    
-    for(int i=0;i<edges.length;i++){
-        adj.get(edges[i][0]).add(new pair(edges[i][1],succProb[i]));
-        adj.get(edges[i][1]).add(new pair(edges[i][0],succProb[i]));
-    }
-    
-    double prob[] = new double[n];
-    PriorityQueue<pair> pq = new PriorityQueue<>((a,b)->{
-        if(b.getValue()>a.getValue()){
-            return 1;
-        }
-        return -1;
-        
-    });
-    Arrays.fill(prob,0);
-    prob[start]=1;
-    pq.offer(new pair(start,1));
-    
-    while(!pq.isEmpty()){
-        int u = pq.poll().getKey();
-        
-        for(pair p :adj.get(u)){
-            int v = p.getKey();
-            double cost = p.getValue();
-            
-            if(prob[v]==0){
-                prob[v] = prob[u]*cost;
-                pq.offer(new pair(v,prob[v]));
-            }else{
-                if(prob[u]*cost>prob[v]){
-                    prob[v] = prob[u]*cost;
-                    pq.offer(new pair(v,prob[v]));
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        double dp[] = new double[n];
+        dp[start] = 1.0;
+        for (int i = 0; i < n - 1; i++) {
+            boolean relaxed = false;
+            for (int j = 0; j < edges.length; j++) {
+                int e1 = edges[j][0];
+                int e2 = edges[j][1];
+                double p = succProb[j];
+                if (dp[e2] * p > dp[e1]) {
+                    dp[e1] = dp[e2] * p;
+                    relaxed = true;
+                }
+                if (dp[e1] * p > dp[e2]) {
+                    dp[e2] = dp[e1] * p;
+                    relaxed = true;
                 }
             }
+            if (!relaxed) break;
         }
+        return dp[end];
     }
-    
-    return prob[end];
-}
 }
