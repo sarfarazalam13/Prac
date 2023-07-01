036f17ee3b243f8223ddf5a5a12938a38f603581
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-01 14:57:40 +0530

    Time: 1 ms (93.23%), Space: 40 MB (58.85%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.java
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.java
@@ -1,23 +1,34 @@
 class Solution {
     int ans = Integer.MAX_VALUE;
+    public int distributeCookies(int[] cookies, int k) {
+        
+        int[] freq = new int[k];
+        backtrack(cookies, freq, 0, k);
 
-    void helper(int[] cookies, int start, int k, int[] temp) {
-        if (start == cookies.length) {
+        return ans;
+    }
+
+    private void backtrack(int[] cookies, int[] freq, int start, int k)
+    {
+        if(start == cookies.length)
+        {
             int max = 0;
-            for (int c : temp)
-                max = Math.max(max, c);
+            for(int f : freq)
+            {
+                max = Math.max(f, max);
+            }
             ans = Math.min(ans, max);
             return;
         }
-        for (int i = 0; i < k; i++) {
-            temp[i] += cookies[start];
-            helper(cookies, start + 1, k, temp);
-            temp[i] -= cookies[start];
+
+        for(int i = 0; i< k; i++)
+        {
+            freq[i] += cookies[start];
+            backtrack(cookies, freq, start + 1, k);
+            freq[i]-=cookies[start];
+            if(freq[i]==0) break;
         }
     }
-
-    public int distributeCookies(int[] cookies, int k) {
-        helper(cookies, 0, k, new int[k]);
-        return ans;
-    }
 }
+
+
