63b1964b6005d8baff620e8941e60748e658be55
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-12 08:22:28 +0530

    Time: 5 ms (98.9%), Space: 55.1 MB (47.92%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,45 +1,42 @@
 class Solution {
-    public boolean DFS(int s, List<List<Integer>> graph, boolean[] visited, boolean[] dfsVisited, boolean[] checkCycle) {
-        visited[s] = dfsVisited[s] = true; // Mark the current node as visited and in the current DFS path
-        for (int it : graph.get(s)) {
-            if (!visited[it]) { // If the adjacent node is not visited
-                if (DFS(it, graph, visited, dfsVisited, checkCycle))
-                    return checkCycle[s] = true; // If a cycle is detected, mark the current node as part of a cycle
-            } else if (dfsVisited[it]) { // If the adjacent node is visited and in the current DFS path
-                return checkCycle[s] = true; // A cycle is detected, mark the current node as part of a cycle
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int V = graph.length;
+
+        boolean[] visited = new boolean[V];
+        boolean[] pathVisited = new boolean[V];
+        boolean[] isSafe = new boolean[V];
+
+        List<Integer> safeNodes = new ArrayList<>();
+
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                dfs(graph, i, visited, pathVisited, isSafe);
             }
         }
-        dfsVisited[s] = false; // Mark the current node as not visited in the current DFS path
-        return false; // No cycle detected, current node is safe
+
+        for(int i=0;i<V;i++){
+            if(isSafe[i]) safeNodes.add(i);
+        }
+
+        return safeNodes;
     }
 
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int v = graph.length;
-        List<List<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i < v; i++) {
-            adjList.add(new ArrayList<>());
-        }
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                adjList.get(i).add(graph[i][j]);
+    public boolean dfs(int[][] adj, int node, boolean[] visited,
+    boolean[] pathVisited,boolean[] isSafe){
+        visited[node] = true;
+        pathVisited[node] = true;
+        
+        for(int it : adj[node]){
+            if(!visited[it]){
+                if(dfs(adj,it,visited,pathVisited,isSafe)){
+                    return true;
+                }
+            }else if(pathVisited[it]){
+                return true;
             }
         }
-
-        boolean[] visited = new boolean[v];
-        boolean[] dfsVisited = new boolean[v];
-        boolean[] checkCycle = new boolean[v];
-        List<Integer> ans = new ArrayList<>();
-
-        for (int i = 0; i < v; i++) {
-            if (!visited[i])
-                DFS(i, adjList, visited, dfsVisited, checkCycle);
-        }
-
-        for (int i = 0; i < v; i++) {
-            if (!checkCycle[i])
-                ans.add(i); // Add the nodes that are not part of any cycle (eventual safe nodes) to the result
-        }
-
-        return ans;
+        isSafe[node] = true;
+        pathVisited[node] = false;
+        return false;
     }
 }
