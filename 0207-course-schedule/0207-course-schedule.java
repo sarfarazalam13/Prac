46cc4131aa8fd39b6a8d7dcc51a8c09bbdf10d6b
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-13 19:08:49 +0530

    Time: 2 ms (99.87%), Space: 43.6 MB (91.6%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,44 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new List[n];
-        int[] indegree = new int[n];
-        List<Integer> ans = new ArrayList<>();
-
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-            if (adj[prerequisite] == null) {
-                adj[prerequisite] = new ArrayList<>();
-            }
-            adj[prerequisite].add(course);
-            indegree[course]++;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(numCourses == 0){
+            return true;
+            // if no courses then it is true
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
+        int[] course = new int[numCourses];
+        // new array length of num of courses
+        for(int i = 0; i<prerequisites.length; i++){
+            course[prerequisites[i][1]]++;
         }
+        // so iterate through and initialise the course array
+        // course array tracks how many times this course is a prerequisite
 
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            ans.add(current);
-
-            if (adj[current] != null) {
-                for (int next : adj[current]) {
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
+        boolean[] visited = new boolean[prerequisites.length];
+        // array to track if each node has been visited
+        boolean flag = true;
+        while(flag){
+            flag = false;
+            for(int i = 0; i<prerequisites.length; i++){
+                if(!visited[i]){
+                    // if node has not been visited
+                    if(course[prerequisites[i][0]]== 0){
+                        visited[i] = true;
+                        course[prerequisites[i][1]]--;
+                        flag = true;
+                        // if the element is not a prerequisite
+                        // mark it as visited node
+                        // decrement value of the course it is dependent on
+                        // flag is set to true to continue iteration
                     }
                 }
             }
         }
-
-        return ans.size() == n;
+        for(int i = 0; i<numCourses; i++){
+            if (course[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    
     }
 }
