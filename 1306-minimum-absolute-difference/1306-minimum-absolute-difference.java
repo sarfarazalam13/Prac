1c352333c1a58cc8e9e87bde208d7984c8732355
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-26 23:53:45 +0530

    Time: 13 ms (99.86%), Space: 57.1 MB (12.7%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
@@ -1,23 +1,44 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        Arrays.sort(arr);
-         List<List<Integer>>ans =new ArrayList<>();
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<arr.length-1;i++)
-        {
-         int diff=arr[i+1]-arr[i];
-
-         if(diff<min)
-         {
-             min=diff;
-             ans.clear();
-             ans.add(Arrays.asList(arr[i],arr[i+1]));
-         }
-         else if(diff==min)
-         {
-             ans.add(Arrays.asList(arr[i],arr[i+1]));
-         }
-
+        countSort(arr);
+        List<List<Integer>> res = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        
+        for (int i=0; i<arr.length-1; i++) {
+            min = Math.min(min, arr[i+1]-arr[i]);
         }
-        return ans;
-}}
+        
+        for (int i=0; i<arr.length-1; i++) {
+            int diff = arr[i+1]-arr[i];
+            if (diff==min) {
+                res.add(Arrays.asList(arr[i], arr[i+1]));
+            }
+        }
+        
+        return res;
+    }
+    
+    private void countSort(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for (int num:nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        
+        boolean[] values = new boolean[max-min+1];
+        
+        for (int num:nums) {
+            values[num-min]=true;
+        }
+        
+        int idx=0;
+        for (int i=0; i<values.length; i++) {
+            if (values[i]) {
+                nums[idx++]=min;
+            }
+            min++;
+        }
+    }
+}
