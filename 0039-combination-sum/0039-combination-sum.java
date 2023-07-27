5577d42d80d78e5014740905f965003d0a299df8
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-27 11:20:07 +0530

    Time: 1 ms (100%), Space: 44.3 MB (21.91%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,22 @@
 class Solution {
-    public void backTrack(List<List<Integer>> result,LinkedList<Integer> lst,int idx,int[] arr,int sum)
-    {
-        if(sum == 0)
-        {
-            result.add(new ArrayList(lst));
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        helper(0,candidates, target,ans,ds);
+        return ans;
+    }
+    public void helper(int index,int arr[],int remain,List<List<Integer>> ans,List<Integer> ds){
+        if(index==arr.length){
+            if(remain==0){
+                ans.add(new ArrayList<>(ds));
+            }
             return;
         }
-        for(int i = idx;i<arr.length;i++)
-        {
-            if(sum - arr[i] >=0)
-            {
-                lst.add(arr[i]);
-                backTrack(result,lst,i,arr,sum-arr[i]);
-                lst.removeLast();
-            }
+        if(arr[index]<=remain){
+            ds.add(arr[index]);
+            helper(index,arr,remain-arr[index],ans,ds);
+            ds.remove(ds.size()-1);
         }
-    }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        LinkedList<Integer> temp = new LinkedList<>();
-        backTrack(result,temp,0,candidates,target);
-        return result;
+        helper(index+1,arr,remain,ans,ds);
     }
 }
