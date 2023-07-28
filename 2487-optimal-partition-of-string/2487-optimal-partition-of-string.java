ced90ece283908dd409658045197b36b4ab132e4
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-28 23:54:56 +0530

    Time: 3 ms (100%), Space: 44.2 MB (77.28%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -1,16 +1,25 @@
 class Solution {
     public int partitionString(String s) {
-        Set<Character> checkUniqueAlphabet = new HashSet<>();
-        int i = 0;
-        int minCount = 0;
-        while (i < s.length()) {
-            minCount++;
-            while (i < s.length() && !checkUniqueAlphabet.contains(s.charAt(i))) {
-                checkUniqueAlphabet.add(s.charAt(i));
-                i++;
+        int sL = s.length();
+        int bitMask = 0;
+        int numParts = 1;
+
+        byte[] chars = new byte[sL];
+        s.getBytes(0, sL, chars, 0);
+
+        for (byte c : chars) {
+            int charMask = 1 << c;
+
+            if ((bitMask & charMask) != 0) {
+                ++numParts;
+                bitMask = charMask;
+            } else {
+                bitMask += charMask;
             }
-            checkUniqueAlphabet.clear();
         }
-        return minCount;
+
+        return numParts;
     }
 }
+/*Time: O(n)
+Space: O(n)*/
