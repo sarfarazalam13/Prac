e3f8412a9e198bac438a5a018eb5e5972a318fe7
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-30 15:12:46 +0530

    Time: 8 ms (100%), Space: 42.4 MB (91.41%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -1,19 +1,55 @@
 class Solution {
+    private int[][] memo;
+    private char[] array;
     public int strangePrinter(String s) {
-        int n = s.length();
-        int[][] dp = new int[n][n];
-        
-        for (int i = n-1; i >= 0; --i) {
-            dp[i][i] = 1;
-            for (int j = i+1; j < n; ++j) {
-                dp[i][j] = dp[i][j-1] + 1;
-                for (int k = i; k < j; ++k) {
-                    if (s.charAt(k) == s.charAt(j)) {
-                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + (k+1<=j-1 ? dp[k+1][j-1] : 0));
-                    }
-                }
+        if (s.isEmpty()) {
+            return 0;
+        }
+        array = squash(s);
+        int N = array.length;
+        memo = new int[N][];
+
+        for (int i = 0; i < N; i++) {
+            memo[i] = new int[N];
+            memo[i][i] = 1;
+            if (i != N - 1) {
+                int next = i + 1;
+                memo[i][next] = array[i] == array[next] ? 1 : 2;
             }
         }
-        return dp[0][n-1];
+        return strangePrinter(0, array.length - 1);
+    }
+    public int strangePrinter(int i, int j) {
+        if (i > j) {
+            return 0;
+        }
+        if (memo[i][j] == 0) {
+
+            int nextIdx = i + 1;
+            int letter = array[i];
+
+            int answer = 1 + strangePrinter(nextIdx, j);
+
+            for (int k = nextIdx; k <= j; k++) {
+                if (array[k] == letter) {
+
+                    int betterAnswer = strangePrinter(i, k - 1) + strangePrinter(k + 1, j);
+                    answer = Math.min(answer, betterAnswer);
+                }
+            }
+            memo[i][j] = answer;
+        }
+        return memo[i][j];
+    }
+    char[] squash(String s) {
+        char[] chars = s.toCharArray();
+        int last = 0;
+        int fullLength = chars.length;
+        for (int i = 1; i < fullLength; i++) {
+            if (chars[i] != chars[last]) {
+                chars[++last] = chars[i];
+            }
+        }
+        return Arrays.copyOf(chars, last + 1);
     }
 }
