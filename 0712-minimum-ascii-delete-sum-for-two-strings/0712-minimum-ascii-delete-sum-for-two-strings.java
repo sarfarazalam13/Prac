a99d7168c6a6a386c15fd9b28b67e97fc8d6a159
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-07-31 11:09:04 +0530

    Time: 7 ms (99.68%), Space: 40.5 MB (98.6%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,36 +1,22 @@
 class Solution {
-    int[][] dp;
-
-    public int minimumDeleteSum(String s1, String s2) {
-        dp = new int[s1.length() + 1][s2.length() + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        return solve(s1, s2, 0, 0);
-    }
-
-    private int solve(String s1, String s2, int i, int j) {
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (i == s1.length()) {
-            int sum = 0;
-            for (int k = j; k < s2.length(); k++) {
-                sum += s2.charAt(k);
+    public int minimumDeleteSum(String string1, String string2) {
+        if(string1.length()<string2.length()) return minimumDeleteSum(string2,string1);
+        char[] s1=string1.toCharArray(), s2=string2.toCharArray();
+        int[] memo=new int[s2.length];
+        memo[memo.length-1]=s2[s2.length-1];
+        for(int i=memo.length-2;i>-1;i--) memo[i]+=memo[i+1]+s2[i];
+        int[] ps=new int[s1.length];
+        ps[ps.length-1]=s1[s1.length-1];
+        for(int i=s1.length-2;i>-1;i--) ps[i]+=ps[i+1]+s1[i];
+        for(int i=s1.length-1;i>-1;i--) {
+            int last=ps[i],prevLast=i<s1.length-1?ps[i+1]:0;
+            for(int j=s2.length-1;j>-1;j--) {
+                int tmp=memo[j];
+                if(s1[i]==s2[j]) last=memo[j]=prevLast;
+                else last=memo[j]=Math.min(memo[j]+s1[i],last+s2[j]);
+                prevLast=tmp;
             }
-            return sum;
         }
-        if (j == s2.length()) {
-            int sum = 0;
-            for (int k = i; k < s1.length(); k++) {
-                sum += s1.charAt(k);
-            }
-            return sum;
-        }
-        int nt = Math.min(s1.charAt(i) + solve(s1, s2, i + 1, j), s2.charAt(j) + solve(s1, s2, i, j + 1));
-        int tk = Integer.MAX_VALUE;
-        if (s1.charAt(i) == s2.charAt(j)) {
-            tk = solve(s1, s2, i + 1, j + 1);
-        }
-        return dp[i][j] = Math.min(nt, tk);
+        return memo[0];
     }
 }
