524cc6b1eed6f27e440098e6751cea080e4ee40c
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-04 14:52:13 +0530

    Time: 8 ms (100%), Space: 76.4 MB (5.03%) - LeetHub

--- 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.java
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.java
@@ -1,17 +1,39 @@
 class Solution {
-    public int maxWidthOfVerticalArea(int[][] points) 
-    {
-        int width=0;
-        int []a=new int[points.length];
-        for(int i=0;i<points.length;i++)
-        {
-            a[i]=points[i][0];
-        }  
-        Arrays.sort(a);
-        for(int i=a.length-1;i>0;i--)
-        {
-            width=Math.max(width,(a[i]-a[i-1]));
+    public int maxWidthOfVerticalArea(int[][] points) {
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int[] point : points) {
+            min = Math.min(min, point[0]);
+            max = Math.max(max, point[0]);
         }
-        return width;
+
+        int diff = max - min;
+
+        if (diff == 0) {
+            return 0;
+        }
+
+        int[][] buckets = new int[points.length + 1][];
+        for (int[] point : points) {
+            int index = (int) ((point[0] - min) * ((long) points.length) / diff);
+           if (buckets[index] == null) {
+                buckets[index] = new int[] {point[0], point[0]};
+            } else {
+                buckets[index][0] = Math.min(buckets[index][0], point[0]);
+                buckets[index][1] = Math.max(buckets[index][1], point[0]);
+            }
+        }
+
+        int maxWidth = 0;
+        int lastMaxX = buckets[0][1];
+        for (int i = 1; i < points.length + 1; i++) {
+            if (buckets[i] != null) {
+                maxWidth = Math.max(maxWidth, buckets[i][0] - lastMaxX);
+                lastMaxX = buckets[i][1];
+            }
+        }
+        return maxWidth;
     }
 }
