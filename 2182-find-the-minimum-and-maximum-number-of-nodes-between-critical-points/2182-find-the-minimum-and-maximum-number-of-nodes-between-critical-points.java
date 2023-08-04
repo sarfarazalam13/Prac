fb511f96f0630c94a857627ef76007c8eaba32bf
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-04 17:24:33 +0530

    Time: 4 ms (100%), Space: 69.5 MB (8.06%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -9,34 +9,30 @@
  * }
  */
 class Solution {
-    public int[] nodesBetweenCriticalPoints(ListNode head) {
-        if(head.next.next==null) return new int[]{-1,-1};
-        int index=2;                 // to see the index it is 2 because 1st node cant be critical point
-        ListNode prev=head;          // point to previous node of current node
-        ListNode curr=head.next;     // to traverse linked list
-        int min=Integer.MAX_VALUE;  //to store first critical point
-        int max=Integer.MIN_VALUE;  // to store last critical point
-        int min_diff=Integer.MAX_VALUE; //to store the minimum distance btw to critical point
-        int prevs=0;               // to store value of previous critical point
-        while(curr.next!=null){
-        if((curr.val>prev.val && curr.val>curr.next.val) || (curr.val<prev.val && curr.val<curr.next.val)){ // condition
-            max=Math.max(max,index); // finding last critical point
-            min=Math.min(min,index); // finding first critical point
-            if(prevs!=0)            //  if this is the second critical point then find its distance with the previous critical point
-                min_diff=Math.min(min_diff,index-prevs);
-            prevs=index;             // shift the previous critical point to the current one
-        }
-        prev=curr;               // update the previous pointer   
-        curr=curr.next;         // move to the next node
-        index++;               // increase the index  
-        }
-       
-        if(min!=Integer.MAX_VALUE && max!=Integer.MIN_VALUE && min!=max){
-            return new int[]{min_diff,max-min};  // max distance is always between first and last critical point and minimum we have already maintained;
-        }
-        else{
-            return new int[]{-1,-1};
-        }
+  public int[] nodesBetweenCriticalPoints(ListNode head) {
+    int minDistance = Integer.MAX_VALUE;
+    int firstMaIndex = -1;
+    int prevMaIndex = -1;
+    int index = 1;
+    ListNode prev = head;      // Point to index 0
+    ListNode curr = head.next; // Point to index 1
 
+    while (curr.next != null) {
+      if (curr.val > prev.val && curr.val > curr.next.val ||
+          curr.val < prev.val && curr.val < curr.next.val) {
+        if (firstMaIndex == -1) // Only assign once
+          firstMaIndex = index;
+        if (prevMaIndex != -1)
+          minDistance = Math.min(minDistance, index - prevMaIndex);
+        prevMaIndex = index;
+      }
+      prev = curr;
+      curr = curr.next;
+      ++index;
     }
+
+    if (minDistance == Integer.MAX_VALUE)
+      return new int[] {-1, -1};
+    return new int[] {minDistance, prevMaIndex - firstMaIndex};
+  }
 }
