175c96497bbd809226a8e70121ac5f56481429db
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-06 12:31:11 +0530

    Time: 21 ms (96.11%), Space: 48.9 MB (58.38%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -13,32 +13,51 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class FindElements {
-    TreeNode root;
-    Set<Integer> set = new HashSet<>();
+    private Set<Integer> values = new HashSet<>();
     public FindElements(TreeNode root) {
-        this.root = root;
-        if(root != null){ root.val = 0; set.add(root.val); }
-        recover(root,set);
+        root.val=0;
+        dfs(root);
+    }
+    private void dfs(TreeNode root){
+        if(root==null) return;
+        int x =root.val;
+        values.add(x);
+        if(root.left!=null) {
+            root.left.val=2*x+1;
+            dfs(root.left);
+        }
+        if(root.right!=null){
+            root.right.val=2*x+2;
+            dfs(root.right);
+        }
     }
     
     public boolean find(int target) {
-      return set.contains(target);
-    }
-
-    private void recover(TreeNode root, Set<Integer> set){
-        if(root.left != null){
-            root.left.val = 2 * root.val + 1;
-            set.add(root.left.val);
-            recover(root.left, set);
-        }
-       if(root.right != null){
-            root.right.val = 2 * root.val + 2;
-            set.add(root.right.val);
-            recover(root.right, set);
-       } 
+        return values.contains(target);
     }
 }
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
 /**
  * Your FindElements object will be instantiated and called as such:
  * FindElements obj = new FindElements(root);
