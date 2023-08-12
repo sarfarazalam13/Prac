8483cb36c0e9a117d6cdc0b709f21ba7d8ef5b6b
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-12 21:30:42 +0530

    Time: 2 ms (100%), Space: 44.4 MB (27.34%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
@@ -14,28 +14,39 @@
  * }
  */
 class Solution {
-    private int mostFrequency = 0;
-    private int dfs(TreeNode root, Map<Integer, Integer> map){
-        if(root == null){
+    int max=Integer.MIN_VALUE;
+    Map<Integer,Integer> map=new HashMap<>();
+    List<Integer> list=new ArrayList<>();
+    public int[] findFrequentTreeSum(TreeNode root) {
+       dfs(root);    
+       
+       
+       int[] arr=new int[list.size()];
+       int j=0;
+       for(int i:list){
+           arr[j++]=i;
+       }
+       return arr;
+
+
+    }
+    public int dfs(TreeNode root){
+        if(root==null){
             return 0;
         }
-        int sum = root.val + dfs(root.left, map) + dfs(root.right, map);
-        int frequency = map.getOrDefault(sum, 0) + 1;
-        map.put(sum, frequency);
-        if(frequency > mostFrequency){
-            mostFrequency = frequency;
+        int left=dfs(root.left);
+        int right=dfs(root.right);
+        int sum=left+right+root.val;
+        int new_val=map.getOrDefault(sum,0)+1;
+        map.put(sum,new_val);
+        if(new_val>max){
+            max=new_val;
+            list.clear();
+            list.add(sum);
+        }
+        else if(new_val==max){
+            list.add(sum);
         }
         return sum;
-    }
-    public int[] findFrequentTreeSum(TreeNode root) {
-        Map<Integer, Integer> map = new HashMap<>();
-        dfs(root, map);
-        List<Integer> list = new ArrayList<>();
-        for(int key : map.keySet()){
-            if(map.get(key) == mostFrequency){
-                list.add(key);
-            }
-        }
-        return list.stream().mapToInt(i -> i).toArray();
     }
 }
