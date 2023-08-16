45d5d866e5e11c2447af9747c2f663a8fceff3cb
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-16 20:07:05 +0530

    Time: 1 ms (100%), Space: 40.9 MB (94.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,34 @@
-class Solution {
-    public String longestPalindrome(String s) {
+class Solution 
+{
+    int start = 0, end = 0;
+    public String longestPalindrome(String s) 
+    {
+        int n = s.length();
+        if (n < 2) return s;
 
-        int start =0,end=0;
-        for(int i=0;i<s.length();i++){
-            
-            int len1= extendPel(s,i,i);
-            int len2=extendPel(s,i,i+1);
-            int len =Math.max(len1,len2);
-//  we can also do (len> end-start) in below if but it will give last occurence of a same length substring and
-//  (len>end-start+1) will give first occurence of same length substring.
-            if(len>(end-start)+1){   
-                start=i-(len-1)/2;
-                end=i+len/2;
-            }
-        }
-        return s.substring(start,end+1);
+        findLongestFrom(s.toCharArray(), 0);
+        
+        return s.substring(start, end + 1);
     }
-    int extendPel(String s,int i,int j){
-        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
-            i--;
-            j++;
+    
+    void findLongestFrom(char[] s, int mid) 
+    {
+        if (s.length - mid < (end - start) / 2) 
+            return;
+
+        int start = mid, end = mid;
+        while (end < s.length - 1 && s[end] == s[end + 1]) end++;
+        mid = end;
+
+        while (start - 1 >= 0 && end + 1 < s.length && s[start - 1] == s[end + 1]) {
+            start--;
+            end++;
         }
-        return j-i-1;  //some people think that this length should be j-i+1 but we need to j-i+1-2 because in last
-                       // itreation we comapred two unmatched charactes so we need to subtract them from length.
+        if (end - start > this.end - this.start) {
+            this.start = start;
+            this.end = end;
+        }
+
+        findLongestFrom(s, mid + 1);
     }
 }
