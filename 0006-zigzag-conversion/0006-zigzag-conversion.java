47db9b34791dfb2fb4f18745176694094912581b
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-18 00:23:32 +0530

    Time: 2 ms (99.93%), Space: 43.4 MB (98.82%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,54 +1,23 @@
 class Solution {
-    public String convert(String s, int numRows) {
-         int col = 0;                  // variable that will store the number of columns the extended matrix have
-        int l = s.length() ;
-        while( l > 0){                // calculating number of columns
-            l -= numRows;
-            col++;
-            for( int i = 0; i < numRows - 2; i++){
-                if( l > 0){
-                    l--;
-                    col++;
-                }
+     public String convert(String s, int numRows) {
+        var length = s.length();
+        if (numRows == 1 || numRows >= length) return s;
+        var memo = new char[length];
+        var step = 2 * numRows - 2;
+        var p = 0;
+        for (var i = 0; i < numRows; i++) {
+            var next = i;
+            while (next < length) {
+                memo[p] = s.charAt(next);
+                next += step;
+                var nextInc = next - 2 * i;
+                if (i > 0 && i < numRows - 1 && nextInc < length)
+                    memo[++p] = s.charAt(nextInc);
+
+                p++;
             }
         }
-        //System.out.println( col);
-        Character m[][] = new Character[ numRows][ col];    // creating matrix to store the asked arrangement
-        l = s.length();
-        int c = 0; int p = 0;
-        while( l > 0){                                     // storing string in the matrix in the asked arrangement
-            //l -= numRows;
-            for( int i = 0; i < numRows; i++){
-                if( l > 0){
-                    l--;
-                    m [i][c] = s.charAt(p++);
-                    //p++;
-                }
-            }
-            c++;
-            for( int i = numRows - 2; i > 0; i--){
-                if( l > 0){
-                    l--;
-                    m [ i][ c++] = s.charAt(p++);
-                    //p++;
-                    //c++;
-                }
-            }
-        }
-        /*for( int i = 0; i < numRows; i++){       // to print the created matrix
-            for( int j = 0; j < col; j++){
-                System.out.print( m[i][j] + " " );
-            }
-            System.out.println("\n");
-        }*/
-        String f = "";                            // traverse the matrix row-wise and answer is achieved
-        for( int i = 0; i < numRows; i++){
-            for( int j = 0; j < col; j++){
-                if( m[i][j] != null){
-                    f += m[i][j];
-                }
-            }
-        }
-        return f;
+
+        return new String(memo);
     }
 }
