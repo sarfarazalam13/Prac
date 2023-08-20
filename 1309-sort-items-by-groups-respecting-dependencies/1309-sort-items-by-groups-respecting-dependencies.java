b0f727da443474d013488df0403ee419c4794bca
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-20 12:02:33 +0530

    Time: 16 ms (100%), Space: 54.4 MB (100%) - LeetHub

--- 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.java
+++ 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.java
@@ -1,94 +1,48 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
     public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
-        Map<Integer, List<Integer>> groupItems = new HashMap<>();
-        int groupId = m;
-        for (int i = 0; i < n; i++) {
-            if (group[i] == -1) {
-                group[i] = groupId;
-                groupId++;
-            }
+        // build graph, group is treated as extra node
+        List<Integer>[] graph = new ArrayList[n+m];
+        int[] indegree = new int[n+m];
 
-            groupItems.computeIfAbsent(group[i], k -> new ArrayList<>()).add(i);
+        for(int i = 0; i < n+m; ++i) graph[i] = new ArrayList<>();
+        
+        for(int i = 0; i < group.length; ++i) {
+            if(group[i] == -1) continue;
+            graph[n+group[i]].add(i);
+            indegree[i]++;
         }
 
-        Map<Integer, List<Integer>> itemGraph = new HashMap<>();
-        Map<Integer, Integer> itemIndegree = new HashMap<>();
+        for(int i = 0; i < beforeItems.size(); ++i) {
+            for(int item : beforeItems.get(i)) {
+                int repBeforeGroup = group[item] == -1 ? item : n+group[item];
+                int repCurrentGroup = group[i] == -1 ? i : n+group[i];
 
-        for (int v = 0; v < n; v++) {
-            for (int u : beforeItems.get(v)) {
-                if (group[u] == group[v]) {
-                    itemGraph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-                    itemIndegree.put(v, itemIndegree.getOrDefault(v, 0) + 1);
+                if(repBeforeGroup == repCurrentGroup){
+                    graph[item].add(i);
+                    indegree[i]++;
+                }else{
+                    graph[repBeforeGroup].add(repCurrentGroup);
+                    indegree[repCurrentGroup]++;
                 }
             }
         }
-
-        Map<Integer, List<Integer>> sortedGroupItems = new HashMap<>();
-
-        for (int groupIdKey : groupItems.keySet()) {
-            List<Integer> sortedItems = topoSort(groupItems.get(groupIdKey), itemGraph, itemIndegree);
-
-            if (sortedItems.size() != groupItems.get(groupIdKey).size()) {
-                return new int[]{};
-            }
-
-            sortedGroupItems.put(groupIdKey, sortedItems);
+        
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < n+m; ++i){
+            //System.out.println(indegree[i]);
+            if(indegree[i] == 0) dfs(graph, indegree, i, n, res);
         }
 
-        Map<Integer, List<Integer>> groupGraph = new HashMap<>();
-        Map<Integer, Integer> groupIndegree = new HashMap<>();
-
-        for (int v = 0; v < n; v++) {
-            for (int u : beforeItems.get(v)) {
-                if (group[u] != group[v]) {
-                    groupGraph.computeIfAbsent(group[u], k -> new ArrayList<>()).add(group[v]);
-                    groupIndegree.put(group[v], groupIndegree.getOrDefault(group[v], 0) + 1);
-                }
-            }
-        }
-
-        Set<Integer> uniqueGroups = new HashSet<>();
-        for (int g : group) {
-            uniqueGroups.add(g);
-        }
-
-        List<Integer> sortedGroups = topoSort(uniqueGroups, groupGraph, groupIndegree);
-
-        if (uniqueGroups.size() != sortedGroups.size()) {
-            return new int[]{};
-        }
-
-        List<Integer> ans = new ArrayList<>();
-
-        for (int sortedGroupId : sortedGroups) {
-            ans.addAll(sortedGroupItems.get(sortedGroupId));
-        }
-
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return res.size() == n ? res.stream().mapToInt(i -> i).toArray() : new int[]{};
     }
 
-    private List<Integer> topoSort(Collection<Integer> nodes, Map<Integer, List<Integer>> graph, Map<Integer, Integer> inDegree) {
-        Queue<Integer> queue = new LinkedList<>(nodes.stream().filter(node -> !inDegree.containsKey(node)).collect(Collectors.toList()));
+    private void dfs(List<Integer>[] graph, int[] indegree, int cur, int n, List<Integer> res) {
+        if(cur < n) res.add(cur);
+        //System.out.println(cur);
+        indegree[cur]--;
 
-        List<Integer> ans = new ArrayList<>();
-
-        while (!queue.isEmpty()) {
-            int curNode = queue.poll();
-            ans.add(curNode);
-
-            if (graph.containsKey(curNode)) {
-                for (int neighbor : graph.get(curNode)) {
-                    inDegree.put(neighbor, inDegree.get(neighbor) - 1);
-                    if (inDegree.get(neighbor) == 0) {
-                        queue.add(neighbor);
-                    }
-                }
-            }
+        for(int child : graph[cur]) {
+            if(--indegree[child] == 0) dfs(graph, indegree, child, n, res);
         }
-
-        return ans;
     }
 }
