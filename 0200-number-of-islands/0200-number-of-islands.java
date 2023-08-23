04973fd1d2bb5e937433e5ef83b8848db1b1f03e
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-24 04:41:30 +0530

    Time: 2 ms (100%), Space: 47.2 MB (86.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,57 +1,29 @@
-class Pair{
-    int first;
-    int second;
-    public Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
-    public void bfs(int row, int col, boolean[][] vis, char[][] grid){
-        vis[row][col] = true;
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(row, col));
-        int n = grid.length;
-        int m = grid[0].length;
-
-        while(!q.isEmpty()){
-            int ro = q.peek().first;
-            int co = q.peek().second;
-            q.remove();
-
-            // traverse in the neighbours and mark them if it is visited
-            for(int delRow = -1; delRow <= 1; delRow++ ){
-                for(int delCol = -1; delCol <= 1; delCol++){
-                    if((delRow==-1 && delCol==0)||(delRow==0 && delCol==1) || (delRow==1 && delCol==0) || (delRow==0 && delCol==-1)){ 
-						/*Striver taught using 8 direction but here in question it has given 
-						only 4 direction*/
-                        int nRow = ro + delRow;
-                        int nCol = co + delCol;
-
-                        if((nRow >= 0 && nRow < n) && (nCol >= 0 && nCol < m) && (vis[nRow][nCol] == false) && 
-                        (grid[nRow][nCol] == '1')){
-                            vis[nRow][nCol] = true;
-                            q.add(new Pair(nRow, nCol));
-                        }
-                    }
-                }
-            }
-        }
+ public static int numIslands(char[][] grid) {
+  List<String> knownLand = new ArrayList<>();
+  int islands = 0;
+  for (int i = 0; i < grid.length; i++) {
+   for (int j = 0; j < grid[i].length; j++) {
+    if(grid[i][j] == '1' ){
+      islands++;
+      exploreIsland(i, j , grid);
     }
-    public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int cnt = 0;
-        boolean[][] vis = new boolean[n][m];
-        
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < m; col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
-                    cnt++;
-                    bfs(row, col, vis, grid);
-                }
-            }
-        }
-        return cnt;
-    }
+   }
+  }
+  return islands;
+ }
+
+private static void exploreIsland(int posA, int posB,char[][] grid){
+  grid[posA][posB] = '2';
+  if(posA-1 >= 0 && grid[posA-1][posB] == '1')
+   exploreIsland(posA-1, posB, grid);
+ if(posA+1 < grid.length && grid[posA+1][posB] == '1')
+  exploreIsland(posA+1, posB, grid);
+ if(posB-1 >= 0 && grid[posA][posB-1] == '1')
+  exploreIsland(posA, posB-1, grid);
+ if(posB+1 < grid[posA].length && grid[posA][posB+1] == '1')
+  exploreIsland(posA, posB+1, grid);
+}
+
+
 }
