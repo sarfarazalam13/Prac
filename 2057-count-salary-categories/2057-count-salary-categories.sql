d30dc59e34112f7806295c87a020c694b9889626
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-26 11:10:37 +0530

    Time: 2650 ms (47.79%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,15 +1,28 @@
-SELECT "Low Salary" AS category,
-       sum(income < 20000) AS accounts_count
-  FROM Accounts
+# Write your MySQL query statement below
 
-UNION
+with salary_category as
+(
+    select 'Low Salary' as category
+    union
+    select 'Average Salary'
+    union
+    select 'High Salary'
+),
+salary_accounts as
+(
+select
+    case 
+        when income < 20000 then 'Low Salary'
+        when income <= 50000 then 'Average Salary'
+        else 'High Salary' end category,
+    count(account_id) as accounts_count
+from Accounts
+group by 1
+)
 
-SELECT "Average Salary" AS category,
-       sum(income BETWEEN 20000 AND 50000) AS accounts_count
-  FROM Accounts
-
-UNION
-
-SELECT "High Salary" AS category,
-       sum(income > 50000) AS accounts_count
-  FROM Accounts;
+select
+    sc.category,
+    coalesce(sa.accounts_count, 0) as accounts_count
+from salary_category sc
+left join salary_accounts sa
+    on sc.category = sa.category
