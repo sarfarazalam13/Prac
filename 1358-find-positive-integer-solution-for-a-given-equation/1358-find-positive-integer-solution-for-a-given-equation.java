d3706c389c398938a7b9a7c7c6862de675ae3fef
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-28 00:07:35 +0530

    Time: 0 ms (100%), Space: 39.9 MB (97.35%) - LeetHub

--- 1358-find-positive-integer-solution-for-a-given-equation/1358-find-positive-integer-solution-for-a-given-equation.java
+++ 1358-find-positive-integer-solution-for-a-given-equation/1358-find-positive-integer-solution-for-a-given-equation.java
@@ -1,32 +1,20 @@
-/*
- * // This is the custom function interface.
- * // You should not implement it, or speculate about its implementation
- * class CustomFunction {
- *     // Returns f(x, y) for any given positive integers x and y.
- *     // Note that f(x, y) is increasing with respect to both x and y.
- *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
- *     public int f(int x, int y);
- * };
- */
-
+ 
 
 class Solution {
     public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 1; i <= 1000; ++i) {
-            int x = 1, y = 1000;
-            while (x < y) {
-                int mid = (x + y) >> 1;
-                if (customfunction.f(i, mid) >= z) {
-                    y = mid;
-                } else {
-                    x = mid + 1;
-                }
+        List<List<Integer>> ans = new ArrayList<>();
+        int i = 1, j = z,k = 1000;
+        while(k --> 0 && i >= 1 && j >= 1) {
+            if(customfunction.f(i, j) == z) {
+                ans.add(Arrays.asList(i, j));
+                i++;
             }
-            if (customfunction.f(i, x) == z) {
-                result.add(Arrays.asList(i, x));
+            else if(customfunction.f(i, j) > z) {
+                j--;
             }
+            else{
+                i++;  }
         }
-        return result;
+        return ans;
     }
 }
