0a051377f13f63e52a9e15068ae3da16bd268507
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-08-31 16:20:33 +0530

    Time: 1 ms (100%), Space: 44 MB (62.77%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,37 +1,47 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        String[] board = new String[n];
-        Arrays.fill(board, ".".repeat(n));
-        int[] leftrow = new int[n];
-        int[] upperDiagonal = new int[2 * n - 1];
-        int[] lowerDiagonal = new int[2 * n - 1];
-        solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
-        return ans;
+      char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List < List < String >> res = new ArrayList < List < String >> ();
+        int leftRow[] = new int[n];
+        int upperDiagonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+        return res;
     }
 
-    private void solve(int col, String[] board, List<List<String>> ans, int[] lr, int[] ud, int[] ld, int n) {
-        if (col == n) {
-            ans.add(new ArrayList<>(Arrays.asList(board)));
+
+
+    static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
+        if (col == board.length) {
+            res.add(construct(board));
             return;
         }
-        for (int row = 0; row < n; row++) {
-            if (lr[row] == 0 && ld[row + col] == 0 && ud[n - 1 + col - row] == 0) {
-                char[] charArray = board[row].toCharArray();
-                charArray[col] = 'Q';
-                board[row] = new String(charArray);
-                lr[row] = 1;
-                ld[row + col] = 1;
-                ud[n - 1 + col - row] = 1;
-                
-                solve(col + 1, board, ans, lr, ud, ld, n);
-                
-                charArray[col] = '.';
-                board[row] = new String(charArray);
-                lr[row] = 0;
-                ld[row + col] = 0;
-                ud[n - 1 + col - row] = 0;
+
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
             }
         }
+    }
+
+
+    static List < String > construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
     }
 }
