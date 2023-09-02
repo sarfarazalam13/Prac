6b67f484e89a23005c9de8e22f05f30cb7390112
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-02 08:23:42 +0530

    Time: 6 ms (100%), Space: 44.3 MB (56%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
@@ -1,32 +1,40 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isWord = false;
+}
+
 class Solution {
     public int minExtraChar(String s, String[] dictionary) {
-        
-         Set<String> dict = new HashSet<>();
-        
-        for (String word : dictionary)
-        {
-            dict.add(word);
+        TrieNode root = new TrieNode();
+        for (String word : dictionary) {
+            insert(root, word);
         }
-
         int n = s.length();
-        
-        int[] dp = new int[n + 1]; 
-        for (int i = 1; i <= n; i++) 
-        {
-            dp[i] = dp[i - 1] + 1; 
-
-            for (int j = i; j >= 1; j--)
-            {
-                String sub = s.substring(j - 1, i);
-
-                if (dict.contains(sub)) 
-                {
-                    dp[i] = Math.min(dp[i], dp[j - 1]); 
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1] + 1;
+            TrieNode node = root;
+            for (int j = i; j < n; j++) {
+                int c = s.charAt(j) - 'a';
+                if (node.children[c] == null) break;
+                node = node.children[c];
+                if (node.isWord) {
+                    dp[i] = Math.min(dp[i], dp[j + 1]);
                 }
             }
         }
+        return dp[0];
+    }
 
-        return dp[n];
-    
+    private void insert(TrieNode root, String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (node.children[i] == null) {
+                node.children[i] = new TrieNode();
+            }
+            node = node.children[i];
+        }
+        node.isWord = true;
     }
 }
