5fd74c8878bd0745e531672e560e6df7c1810e0b
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-06 00:50:04 +0530

    Time: 6 ms (99.26%), Space: 44.3 MB (43.75%) - LeetHub

--- 1767-design-front-middle-back-queue/1767-design-front-middle-back-queue.java
+++ 1767-design-front-middle-back-queue/1767-design-front-middle-back-queue.java
@@ -1,49 +1,119 @@
 class FrontMiddleBackQueue {
-    LinkedList<Integer>queue;
+    class Node{
+        Node next,prev;
+        int val;
+        Node(int val){
+            this.val=val;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    Node head,tail,mid;
+    int sz;
     public FrontMiddleBackQueue() {
-        queue = new LinkedList<>();
+        this.head=new Node(-1);
+        this.tail=new Node(-1);
+        this.head.next=tail;
+        this.tail.prev=head;
+        this.mid=head;
+        this.sz=0;
     }
     
     public void pushFront(int val) {
-        queue.add(0,val);
+        Node node=new Node(val);
+        node.next=head.next;
+        node.next.prev=node;
+        node.prev=head;
+        head.next=node;
+        if(sz==0){
+            mid=node;
+        }
+        else if(sz%2==1){
+            mid=mid.prev;
+        }
+        sz++;
     }
     
     public void pushMiddle(int val) {
-        int n = queue.size();
-        int mid = (n) / 2;
-        queue.add(mid , val);     
+        if(sz==0){
+            pushFront(val);
+        }
+        else{
+            Node node=new Node(val);
+            if(sz%2==1){
+                mid.prev.next=node;
+                node.prev=mid.prev;
+                node.next=mid;
+                mid.prev=node;
+            }
+            else{
+                node.next=mid.next;
+                mid.next=node;
+                node.prev=mid;
+                node.next.prev=node;
+            }
+            mid=node;
+            sz++;
+        }
+
     }
     
     public void pushBack(int val) {
-        queue.addLast(val);
-    }
-    
-    public int popFront() {
-        if(queue.isEmpty())
-        {
-            return -1;
+        if(sz==0){
+            pushFront(val);
         }
-        return queue.removeFirst();
+        else{
+            Node node=new Node(val);
+            tail.prev.next=node;
+            node.prev=tail.prev;
+            node.next=tail;
+            tail.prev=node;
+            if(sz%2!=1){
+                mid=mid.next;
+            }
+            sz++;
+        }
+
+
+    }
+    public int popFront() {
+        if(sz==0)return -1;
+        int x=head.next.val;
+        if(sz%2==0){
+            mid=mid.next;
+        }
+        head.next=head.next.next;
+        head.next.prev=head;
+        sz--;
+        return x;
     }
     
     public int popMiddle() {
-        if(queue.isEmpty())
-        {
-            return -1;
+        if(sz==0)return -1;
+        int m=mid.val;
+        Node temp=mid;
+        if(sz%2==1){
+            mid=mid.prev;
         }
-        int n = queue.size();
-        int mid = (n - 1) / 2;
-        int pm = queue.remove(mid);
-        return pm;
+        else{
+            mid=mid.next;
+        }
+        temp.prev.next=temp.next;
+        temp.next.prev=temp.prev;
+        sz--;
+        return m;
     }
     
     public int popBack() {
-         if(queue.isEmpty())
-        {
-            return -1;
+        if(sz==0)return -1;
+        int x=tail.prev.val;
+        if(sz%2!=0){
+            mid=mid.prev;
         }
-       int pb =  queue.removeLast();
-       return pb;
+        tail.prev=tail.prev.prev;
+        tail.prev.next=tail;
+        sz--;
+        return x;
     }
 }
 
