1568f39b6eec7d6a4bba0955db28f5b054b7d21f
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-10 00:27:19 +0530

    Time: 11 ms (100%), Space: 55.9 MB (20.45%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
@@ -13,32 +13,51 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-        HashMap<Integer, TreeNode> map = new HashMap<>();
-        Set<Integer> children = new HashSet<>();
-        for (int[] arr : descriptions) {
-            int parent = arr[0], child = arr[1], isLeft = arr[2];
-            children.add(child);
-            TreeNode node = map.getOrDefault(parent, new TreeNode(parent));
-            if (isLeft == 1) {
-                node.left = map.getOrDefault(child, new TreeNode(child));
-                map.put(child, node.left);
-            } else {
-                node.right = map.getOrDefault(child, new TreeNode(child));
-                map.put(child, node.right);
+        TreeNode [] nodes=new TreeNode[100001];
+        int []count=new int[100001];
+        for(int []arr:descriptions){
+            int par=arr[0];
+            int chi=arr[1];
+            boolean isl=(arr[2]==1);
+            if(nodes[par]==null){
+                nodes[par]=new TreeNode(par);
             }
-            map.put(parent, node);
-        }
-        
-        int root = -1;
-        for (int [] arr: descriptions) {
-            if (!children.contains(arr[0])) {
-                root = arr[0];
-                break;
+            if(nodes[chi]==null){
+                nodes[chi]=new TreeNode(chi);
+            }
+            TreeNode parent=nodes[par];
+            TreeNode children=nodes[chi];
+            if(isl){
+                parent.left=children;
+            }
+            else{
+                parent.right=children;
+            }
+            count[chi]=-1;
+            if(count[par]>=0){
+                count[par]=1;
             }
         }
-        
-        return map.getOrDefault(root, null);
+        for(int i=1;i<=100001;i++){
+            if(count[i]==1)return nodes[i];
+        }
+        return null;
     }
 }
