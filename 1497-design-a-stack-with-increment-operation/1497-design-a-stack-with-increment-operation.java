4e8fabbad71722bccc7ce484fc8edb9f13bee0b3
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-11 02:26:51 +0530

    Time: 5 ms (96.18%), Space: 44.1 MB (76.77%) - LeetHub

--- 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.java
@@ -1,33 +1,39 @@
 class CustomStack {
-    private int[] stack;
-    private int currIndex;
+    int[] stack;
+    int currIndex;
     int maxSize;
     public CustomStack(int maxSize) {
-        this.currIndex=0;
-        this.stack= new int[maxSize];
-        this.maxSize= maxSize;
-        
+        this.currIndex = 0;
+        this.stack = new int[maxSize];
+        this.maxSize = maxSize;
     }
     
     public void push(int x) {
-        if(currIndex< maxSize) stack[currIndex++]=x;
-        
+        if (currIndex < maxSize) {
+            stack[currIndex++] = x;
+        }  
     }
     
     public int pop() {
-        if(currIndex ==0) return -1;
-        else return stack[--currIndex];
-        
+        if (currIndex == 0) return -1;
+        return stack[--currIndex];
     }
     
     public void increment(int k, int val) {
-        int n= Math.min(currIndex, k);
-        for(int i=0;i<n;i++){
-            stack[i]+=val;
+        k = Math.min(k, maxSize);
+        for (int i = 0; i < k; i++) {
+            stack[i] += val;
         }
     }
 }
 
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack obj = new CustomStack(maxSize);
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * obj.increment(k,val);
+ */
 /**
  * Your CustomStack object will be instantiated and called as such:
  * CustomStack obj = new CustomStack(maxSize);
