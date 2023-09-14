5ec34431f6b5d9578fa8450cc34434eb6b82a40c
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-14 22:08:28 +0530

    Time: 5 ms (96.93%), Space: 44.3 MB (79.52%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,22 +1,40 @@
 class Solution {
+    // Since the answer has to be sorted lexicographically, we will sort the inputs and the first valid answer we get will be the final answer
+    
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> graph = new HashMap<>();
-        
-        for (List<String> ticket : tickets) {
-            graph.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>()).add(ticket.get(1));
+        // asnwer will have a size of tickets+1
+        LinkedList<String> ans =new LinkedList<>(); // because we will need the insert at begining function.
+
+        // We will use hashmap to store the adjacency list
+        // we will use a priority queue for sorting 
+        HashMap<String,PriorityQueue<String>> adj=new HashMap<>();
+
+        // Initializing
+        for(List<String> ticket:tickets){
+            String u=ticket.get(0);
+            String v=ticket.get(1);
+
+            adj.putIfAbsent(u,new PriorityQueue<String>());
+            adj.get(u).add(v);
         }
-        
-        LinkedList<String> stack = new LinkedList<>();
-        stack.add("JFK");
-        LinkedList<String> itinerary = new LinkedList<>();
-        
-        while (!stack.isEmpty()) {
-            while (graph.getOrDefault(stack.peekLast(), new PriorityQueue<>()).size() > 0) {
-                stack.add(graph.get(stack.peekLast()).poll());
-            }
-            itinerary.addFirst(stack.pollLast());
+
+        // Starting location will always be JFK
+        // Ending can be anything
+
+        // We will start computing from jfk and for every destination we go to we remove that link from our adj list
+        // if an answer cannot be formed we will backtrack and add that link again
+
+        dfs("JFK",adj,ans);
+        return ans;
+    }
+    public void dfs(String src,HashMap<String,PriorityQueue<String>> adj,LinkedList<String> ans){
+        PriorityQueue<String> temp=adj.get(src);
+
+        while(temp!=null && temp.size()>0){
+            String s=temp.poll();
+            dfs(s,adj,ans);
         }
-        
-        return itinerary;
+        ans.addFirst(src);
+        return;
     }
 }
