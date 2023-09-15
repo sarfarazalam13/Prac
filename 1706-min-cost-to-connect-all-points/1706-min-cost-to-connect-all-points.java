da10e2dbc735552da945dd1de3ba63ddbff4bfd3
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-15 10:49:13 +0530

    Time: 8 ms (100%), Space: 43.5 MB (92.34%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,47 +1,68 @@
+// not prior for now 
+
 class Solution {
+    
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        int[][] matrix = new int[n][n];
-        boolean [] visited = new boolean[n];
-        Queue <Integer> queue = new LinkedList<>();
-        int distance =0;
-        // get all distance 
-        for(int i =0; i < n; i++){
-            for(int j =0; j<n; j++){
-               matrix[i][j] = Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
-            };
-        }
-
-
-        // first 
-        queue.add(0);
-        visited[0] = true; 
-        while(queue.size() < n){
-          int sizes = queue.size();
-          int len = Integer.MAX_VALUE;
-          int choosen =0;
-          int nextPoint=0;
-          for(int ss =0; ss < sizes; ss++){
-                int connect = queue.poll();
-                for(int next =0; next < n; next ++){
-                if(!visited[next]){
-                    if(matrix[connect][next]<len){
-                    choosen = connect; 
-                    nextPoint = next;
-                    len = matrix[connect][next];
-                    }  
-                }
-
-                 }
-                 queue.add(connect);
-           }
-          visited[nextPoint] = true; 
-          queue.add(nextPoint);
-          distance += len; 
-
-        }
-       
-     return distance;  
+        int dist = 0, pointer = 0;
+        Point[] ps = initPoints(points);
         
+        while (pointer < points.length) {
+            Point point = ps[pointer++];
+            
+            dist += point.dist == Integer.MAX_VALUE ? 0 : point.dist;
+            
+            int nextPoint = findNextPoint(ps, point, pointer);
+            
+            if (nextPoint > -1)
+                swap(ps, nextPoint, pointer);
+        }
+        
+        return dist;
+    }
+    
+    private class Point {
+        int x, y, dist;
+        
+        public Point(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+    
+    private Point[] initPoints(int[][] points) {
+        Point[] ps = new Point[points.length];
+        
+        for (int i = 0; i < points.length; i++) {
+            int[] p = points[i];
+            
+            ps[i] = new Point(p[0], p[1], Integer.MAX_VALUE);
+        }
+        
+        return ps;
+    }
+    
+    private int findNextPoint(Point[] ps, Point point, int start) {
+        int idx = -1, min = Integer.MAX_VALUE;
+        
+        for (int i = start; i < ps.length; i++) {
+            int m = Math.abs(point.x - ps[i].x) + Math.abs(point.y - ps[i].y);
+            
+            ps[i].dist = Math.min(ps[i].dist, m);
+            
+            if (ps[i].dist < min) {
+                min = ps[i].dist;
+                idx = i;
+            }
+        }
+        
+        return idx;
+    }
+    
+    private void swap(Point[] ps, int i, int j) {
+        Point tmp = ps[i];
+        
+        ps[i] = ps[j];
+        ps[j] = tmp;
     }
 }
