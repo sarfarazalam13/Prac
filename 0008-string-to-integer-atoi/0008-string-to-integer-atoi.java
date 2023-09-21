0877bd045259142051e06a73fe396676260c7fd6
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-21 18:38:45 +0530

    Time: 2 ms (47.09%), Space: 40.9 MB (98.38%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,26 +1,43 @@
-
 class Solution {
-  public int myAtoi(String s) {
-    s = s.strip();
-    if (s.isEmpty())
-      return 0;
+    public int myAtoi(String s) {
 
-    final int sign = s.charAt(0) == '-' ? -1 : 1;
-    if (s.charAt(0) == '+' || s.charAt(0) == '-')
-      s = s.substring(1);
 
-    long num = 0;
+        int num = 0;
+        int neg = 1;
+        int i = 0;
+        for(char c : s.toCharArray()){
+            if(c != ' ' )
+                i++;
+          
+            if(num ==0 ){
+                if( c == '-' || c == '+' ){
+                    neg = c == '-' ? -1 : 1;
+                
+                    if(i > 1)
+                        return 0;
+                    continue;
+                }
+            }
+            if(Character.isDigit(c)){
 
-    for (final char c : s.toCharArray()) {
-      if (!Character.isDigit(c))
-        break;
-      num = num * 10 + (c - '0');
-      if (sign * num <= Integer.MIN_VALUE)
-        return Integer.MIN_VALUE;
-      if (sign * num >= Integer.MAX_VALUE)
-        return Integer.MAX_VALUE;
+                int digit=  c - '0';
+                if(Integer.MAX_VALUE/10 < num || Integer.MAX_VALUE/10 == num && Integer.MAX_VALUE %10 < digit){
+                    return neg == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE ;
+
+                }
+               
+                num = num * 10 + c - '0';
+            }
+            else if(!Character.isDigit(c) && c != ' ' && num != 0 ){
+                return num * neg;
+            } else if(c != ' '){
+                return num * neg;
+            } else if(c == ' ' && i >= 1){
+                return num*neg;
+            }
+            
+        }
+        return num * neg;
+        
     }
-
-    return sign * (int) num;
-  }
 }
