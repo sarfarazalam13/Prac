e4cf2464eb17ba6db0fcfbbbd0b19de3c09b13d1
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-09-27 02:41:47 +0530

    Time: 222 ms (5.56%), Space: 45.9 MB (66.67%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,102 +1,67 @@
-class TopNode {
-    int x;
-    int h;
-    TopNode next;
-    TopNode() {
-    }
-    TopNode(int x, int h) {
-        this.x = x;
-        this.h = h;
-    }
-
-    void insert(TopNode n) {
-        n.next = next;
-        next = n;
-    }
-
-}
 class Solution {
-    static final int LEFT=0, RIGHT=1, HEIGHT=2;
+    class Node {
+        int index;
+        int height;
+        Node(int index, int height){
+            this.index = index;
+            this.height = height;
+        }
+    }
+    class sortBy implements Comparator<Node> {
+        public int compare(Node a, Node b){
+            if(a.index-b.index>0){
+                return 1;
+            } else if(a.index==b.index){
+                return a.height-b.height;
+            }
+            return a.index-b.index;
+        }
+    }
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        TopNode head = new TopNode(0,0);
-        head.insert(new TopNode(Integer.MAX_VALUE, 0));
-        TopNode start = head;
-        
-        for (int i = 0; i<buildings.length; i++) {
-            int[] b = buildings[i];
-            int bL = buildings[i][LEFT];
-            int bR = buildings[i][RIGHT];
-            int bH = buildings[i][HEIGHT];
-           
-            while (bL >= start.next.x) { start = start.next; } 
-            
-            for (TopNode t = start ; bR > t.x; t = t.next) {
-               
-                if (bH <= t.h) {
-                    continue;
+        List<Node> list = new ArrayList<Node>();
+        int height = 0;
+        for(int i=0; i<buildings.length; i++){
+            list.add(new Node(buildings[i][0], -1*buildings[i][2]));
+            list.add(new Node(buildings[i][1], buildings[i][2]));
+        }
+        Collections.sort(list, new sortBy());
+        List<Integer> byHeight = new ArrayList<Integer>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        while(list.size()>0){
+            Node temp = list.remove(0);
+            if(temp.height<0) {
+                byHeight.add(temp.height*-1);
+                if(temp.height*-1>height){
+                    height = temp.height*-1;
+                    List<Integer> ans = new ArrayList<Integer>();
+                    ans.add(temp.index);
+                    ans.add(temp.height*-1);
+                    result.add(ans);
+                } 
+            } else {
+                byHeight.remove(byHeight.indexOf(temp.height));
+                if(temp.height==height){
+                    int newHeight = byHeight.size()>0 ? byHeight.get(biggest(byHeight)) : 0;
+                    if(newHeight!=height){
+                        List<Integer> ans = new ArrayList<Integer>();
+                        ans.add(temp.index);
+                        ans.add(newHeight);
+                        result.add(ans);
+                    }
+                    height = newHeight;
                 }
-
-                TopNode stop = t;
-                while (stop.next != null && stop.next.x < bR && stop.next.h <= bH ) {
-                    stop = stop.next;
-                }
-                
-                if (bL <= t.x) {
-                    if (bR >= stop.next.x) {
-                        t.next = stop.next;
-                        t.h = bH;
-                    }
-                    else if (t == stop) {
-                        t.insert(new TopNode(bR,t.h));
-                        t.h = bH;
-                        break;
-                    }
-                    else {
-                        stop.x = bR;
-                        t.h = bH;
-                        t.next = stop;
-                        break;
-                    }
-                }
-                else {
-                    if (bR >= stop.next.x) {
-                        if (t == stop) {
-                            t.insert(new TopNode(bL, bH));
-                        }
-                        else {
-                            t.next = stop;
-                            stop.x = bL;
-                            stop.h = bH;
-                        }
-                        break;
-                    }
-                    else if (t == stop) {
-                        t.insert(new TopNode(bL,bH));
-                        t.next.insert(new TopNode(bR,t.h));
-                        break;
-                    }
-                    else {
-                        t.next = stop;
-                        t.insert(new TopNode(bL,bH));
-                        stop.x = bR;
-                        break;
-                    }
-                }
-                t = stop;
-
             }
         }
-
-        List<List<Integer>> skyline = new ArrayList<>();
-
-        if (head.h == 0)
-            head = head.next;
-        while (head != null) {
-            int height = head.h;
-            skyline.add(List.of(head.x, height));
-            while ( (head = head.next) != null && head.h == height) {}
+        return result;
+    }
+    public int biggest(List<Integer> list){
+        int max = -1, index = -1;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i)>max){
+                max = list.get(i);
+                index = i;
+            }
         }
-
-        return skyline;
+        return index;
     }
 }
