fb32ba84264f79dab8a2c5298baee9281b1916fb
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-10-11 08:57:24 +0530

    Time: 20 ms (100%), Space: 71.1 MB (65.77%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.java
@@ -1,58 +1,52 @@
 class Solution {
-    public int[] fullBloomFlowers(int[][] flowers, int[] persons) {
-        int n = flowers.length;
-        int[] start = new int[n];
-        int[] end = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            start[i] = flowers[i][0];
-            end[i] = flowers[i][1];
-        }
-        
-        Arrays.sort(start);
-        Arrays.sort(end);
-        
-        int[] res = new int[persons.length];
-        
-        for (int i = 0; i < persons.length; i++) {
-            int t = persons[i];
-            int started = binarySearchUpperBound(start, t);
-            int ended = binarySearchLowerBound(end, t);
-            res[i] = started - ended;
-        }
-        
-        return res;
-    }
+public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+
+  int m = flowers.length;
+  int[] starts = new int[m], ends = new int[m];
+  for (int i = 0; i < m; i++) {
+      starts[i] = flowers[i][0];
+      ends[i] = flowers[i][1];
+  }
+  Arrays.sort(starts);
+  Arrays.sort(ends);
+
+  int n = people.length;
+  
+  int[] indices = new int[n];
+  for (int i = 0; i < n; i++) 
+      indices[i] = i;
+  
+  quickSort(indices, people, 0, n - 1);
+  
+  int[] result = new int[n];
+  int index1 = 0, index2 = 0;
+  for (int index:indices) {
+      int arrival = people[index];
+      while ( index1 < m && starts[index1] <= arrival) index1++;
+      while ( index2 < m && ends[index2] < arrival) index2++;  
+      result[index] = index1 - index2;
+  }
+  return result;
+
+}
+
+public void quickSort(int[] indices, int[] people, int low, int high) {
     
-    private int binarySearchUpperBound(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+    if (low >= high) return;
+
+    int left = low -1, right = high + 1, mid = low + high >> 1;
+    int pivot = people[indices[mid]];
+
+    while (left  < right) {
+        while (people[indices[++left]] < pivot) continue;
+        while (people[indices[--right]] > pivot) continue;
+        if (left < right) {
+            int temp = indices[left];
+            indices[left] = indices[right];
+            indices[right] = temp;
         }
-        
-        return left;
     }
-    
-    private int binarySearchLowerBound(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        return left;
-    }
+    quickSort(indices, people, low, right); 
+    quickSort(indices, people, right + 1, high);
+  }
 }
