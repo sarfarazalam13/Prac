6abab600182a8d241a892d1b5b47aa45134210b8
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-10-19 17:09:53 +0530

    Time: 0 ms (100%), Space: 44.1 MB (63.67%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,29 +1,59 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int min = 1;
-        int max = Integer.MIN_VALUE;
+    //     int low=1;
+    //     int high=findMax(arr);
 
-        for(int a : piles) {
-            max = Math.max(max,a);
+    //     while(low<=high){
+    //        int mid=(low+high)/2;
+    //         int totalhr=totalHr(arr,mid);
+    //         if(totalhr<=h){
+    //             high=mid-1;
+    //         }else{
+    //             low=mid+1;
+    //         }
+    //     }
+    //     return low;
+        
+    // }
+    // public int findMax(int[] arr){
+    //     int n=arr.length;
+    //     int max=Integer.MIN_VALUE;
+    //     for(int i=0;i<n;i++){
+    //         max=Math.max(max,arr[i]);
+            
+    //     }
+    //     return max;
+    // }
+    // public int totalHr(int[] arr,int h){
+    //     int totalh=0;
+    //     int n=arr.length;
+    //     for(int i=0;i<n;i++){
+    //         totalh += Math.ceil((double)(arr[i]) / (double)(h));
+    //     }
+    //     return totalh;
+     int n = piles.length;
+        long s = 0;
+        for (int pile : piles) {
+            s += pile;
         }
 
-        while(min < max) {
-            int mid = (min+max)/2;
-            if(getHoursToEat(piles, mid) > h) {
-                min = mid + 1;
+        int left = (int) ((s - 1) / h) + 1;
+        int right = (int) ((s - n) / (h - n + 1) + 1);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            long totalTime = 0;
+            for (int pile : piles) {
+                totalTime += ((pile - 1) / mid + 1);
+            }
+
+            if (totalTime <= h) {
+                right = mid;
             } else {
-                max = mid;
+                left = mid + 1;
             }
         }
 
-        return min;
-    }
-
-    private int getHoursToEat(int[] piles, int k) {
-        int h = 0;
-        for(int a : piles) {
-            h += Math.ceil((double)a/k);
-        }
-        return h;
+        return left;
     }
 }
