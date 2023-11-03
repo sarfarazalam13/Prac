64373bc08038cdd9dc462789342e09296d954d76
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2023-11-03 20:38:50 +0530

    Time: 20 ms (23.8%), Space: 45.8 MB (77.81%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,22 +1,19 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> matrix[a[0]][a[1]] - matrix[b[0]][b[1]]);
-        
-        for (int i = 0; i < n; i++) {
-            minHeap.offer(new int[]{i, 0});
-        }
-        
-        for (int i = 0; i < k - 1; i++) {
-            int[] cur = minHeap.poll();
-            int row = cur[0];
-            int col = cur[1];
-            if (col < n - 1) {
-                minHeap.offer(new int[]{row, col + 1});
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+              pq.add(matrix[i][j]);
+             
+             if(pq.size()>k)
+             {
+                 pq.remove();
+             }
             }
         }
-        
-        int[] kthSmallestElement = minHeap.poll();
-        return matrix[kthSmallestElement[0]][kthSmallestElement[1]];
+       
+        return pq.remove();
     }
 }
