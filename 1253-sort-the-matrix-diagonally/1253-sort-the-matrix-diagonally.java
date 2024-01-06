c81fe361c845172ef696623db6ea34d973477ea6
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2024-01-06 23:07:12 +0530

    Time: 1 ms (100%), Space: 44.7 MB (27.29%) - LeetHub

--- 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
@@ -1,35 +1,39 @@
 class Solution {
+    public void sum(int[] count) {
+        for(int i = 1; i < count.length; i++) {
+            count[i] = count[i] + count[i-1];
+        }
+    }
+    
+    public void sort(int[][] mat, int[][] res, int a, int b) {
+        int[] count = new int[101];
+        int i = a, j = b;
+        while(i < mat.length && j < mat[0].length) {
+            count[mat[i][j]]++;
+            i++; j++;
+        }
+        sum(count);
+        i = a; j = b;
+        while(i < mat.length && j < mat[0].length) {
+            int inx = --count[mat[i][j]] ;
+            res[a+inx][b+inx] = mat[i][j];
+            i++; j++;
+        }
+    }
+    
     public int[][] diagonalSort(int[][] mat) {
-        if(mat.length == 1) {
-            return mat;
-        }
-
-        int i = mat.length - 2;
-        int j = 0;
-
-        while(i != 0 || j != mat[0].length - 1) {
-            List<Integer> list = new ArrayList<>();
-            int m = i;
-            int n = j;
-
-            while(m < mat.length && n < mat[0].length) {
-                list.add(mat[m++][n++]);
-            }
-
-            Collections.sort(list);
-
-            m = i;
-            n = j;
-            for(int l = 0; l < list.size(); l++) {
-                mat[m++][n++] = list.get(l);
-            }
-
-            if(i != 0) {
-                i--;
+        int[][] res = new int[mat.length][mat[0].length];
+        
+        int a = mat.length-1;
+        int b = 0;
+        while(b < mat[0].length) {
+            sort(mat, res, a, b);
+            if (a < 1) {
+                b++;
             } else {
-                j++;
+                a--;
             }
         }
-        return mat;
+        return res;
     }
 }
