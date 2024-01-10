25345c335b7633ea258082c81863b35372bf6c92
Sarfaraz Alam <56781879+sarfarazalam13@users.noreply.github.com>
2024-01-11 02:08:38 +0530

    Time: 24 ms (100%), Space: 60.2 MB (20.25%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
@@ -1,28 +1,28 @@
 class Solution {
-    boolean check(int[] arr,int m,int o)
-    {
-        for(int i=0;i<arr.length;i++)
-        {
-            o-=(arr[i]/m);
-            if(arr[i]%m==0)o++;
-        }
-        return o>=0;
-    }
     public int minimumSize(int[] nums, int maxOperations) {
-       int l=1,r=Integer.MAX_VALUE,ans=0; 
-       while(l<=r)
-       {
-         int m=l+(r-l)/2;
-         if(check(nums,m,maxOperations))
-         {
-             ans=m;
-             r=m-1;
-         }
-         else
-         {
-          l=m+1;
-         }
-       }
-       return ans;
+        int min = 1;
+        int max = 1000000000;
+        while (min < max) {
+            int mid = (max + min) / 2;
+            if (isMidPossible(nums, maxOperations, mid)) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return min;
+    }
+
+    public boolean isMidPossible(int[] nums, int maxOperations, int mid) {
+        int result = 0;
+        for (int i : nums) {
+            // compression - make use of divide property to implicitly check modulo case
+            result += (i - 1) / mid;
+            if (result > maxOperations) {
+                // short-circuiting
+                return false;
+            }
+        }
+        return true;
     }
 }
